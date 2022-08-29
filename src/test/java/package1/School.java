609c3d0bb7ff7de22c82e7a8401d4a66ab14845d
Dell <Dell@DESKTOP-16O7D5M>
2022-08-29 12:15:55 +0530

    commit

---
+++ src/test/java/package1/School.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class School {
+	@Test
+	public void sch()
+	{
+		System.out.println("camlin");
+	}
+
+}
---
+++ src/test/java/package2/Collage.java
@@ -0,0 +1,11 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class Collage {
+	@Test
+	public void coll()
+	{
+		System.out.println("PES");
+	}
+}
